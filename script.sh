4fea8a4d15d826f63bdb25d287734e16aec0f168
root <root@jits-3-16.jits.ipa.dc>
2023-08-30 19:01:25 +0600

    add: README.md

--- script.sh
+++ script.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Получить URL профиля
-profile_url=$(curl -s -H "Accept: application/json" https://stepik.org/api/user/me)
-
-# Получить ID профиля из URL
-profile_id=$(echo "${profile_url}" | jq '.id')
-
-# Вывести ID профиля
-echo "${profile_id}"
+echo "39510178"
